6837def3f1de37ae1006f855950bfea7d7c9eddc
ABHISHEK KUMAR <abhishek9471426188@gmail.com>
2026-08-10 08:30:37 +0000

    datatype

---
+++ BASICS/02_datatypes.js
@@ -0,0 +1,16 @@
+let name="abhiman";
+console.log(typeof name);
+
+let age=78;
+console.log(typeof age);
+
+let flag=true;
+console.log(typeof flag);
+
+let nick_name=null;
+console.log(typeof nick_name);// object;
+
+let branch;
+console.log(typeof branch);// undefined is a datatpe but 
+
+//datatype -->symbol : unique
