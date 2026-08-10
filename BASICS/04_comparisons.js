a5b6b43c7091092b04e77938eeaa6064634cdd19
ABHISHEK KUMAR <abhishek9471426188@gmail.com>
2026-08-10 11:34:58 +0000

    comparisons

---
+++ BASICS/04_comparisons.js
@@ -0,0 +1,29 @@
+//different comparisons operators;
+//convert strings into number and after that compare
+{
+console.log(5=="5")//equal value;//true
+console.log(5==5)//equal value ;//true
+
+console.log(5!=6)//true
+console.log(5!="6")//true
+
+console.log(5>="4")//true;
+console.log(5>=4)//true
+
+console.log(5<="6" && 5<=6)//true
+
+
+
+
+}
+
+//compare both datatype and value;
+
+{
+console.log(5==="5") //equal value and same datatype; //false
+console.log(5===5) //equal value and same datatype; // true
+
+console.log(5!==6)//true
+console.log(5!=="6")//true
+console.log(5!=="5")//true
+}
